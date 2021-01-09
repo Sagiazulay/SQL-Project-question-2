a2f9e2313825580f90777a4f523ab3ef87292c5c
Sagiazulay <58825185+Sagiazulay@users.noreply.github.com>
2021-01-09 18:41:22 +0200

    Add files via upload

---
+++ Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlProject
+{
+    class Tests
+    {
+        public int ID { get; set; }
+
+        public int Car_id { get; set; }
+
+        public int Is_passed { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public Tests()
+        {
+
+        }
+
+        public Tests(int iD, int car_id, int is_passed, DateTime timestamp)
+        {
+            ID = iD;
+            Car_id = car_id;
+            Is_passed = is_passed;
+            Timestamp = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Newtonsoft.Json.JsonConvert.SerializeObject(this).Replace(",", "\n")}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.ID == ((Tests)obj).ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)ID;
+        }
+        public static bool operator ==(Tests test1, Tests test2)
+        {
+            return test1.ID == test2.ID;
+        }
+        public static bool operator !=(Tests test1, Tests test2)
+        {
+            return !(test1 == test2);
+        }
+    }
+}
---
+++ TestsDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlProject
+{
+    class TestsDAO
+    {
+        private static readonly log4net.ILog my_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private string m_conn_string = @"Data Source=C:\\Users\\Sagi\\.dotnet\\misrad.db;Version=3;";
+        public List<Tests> CarsByTests = new List<Tests>();
+        public void GetTestsWithCars(int tstid)
+        {
+                    
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                using (cmd.Connection = new SqlConnection(m_conn_string))
+                {
+                    Tests result;
+                    cmd.Connection.Open();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = $"SELECT Cars.ID, Cars.Manufacturer, Cars.Model, Cars.Year" +
+                                        "FROM Cars" +
+                                        "JOIN Tests ON Cars.ID = Tests.Car_ID; ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        result = new Tests
+                        {
+                            ID = (int)reader["ID"],
+                            Car_id = (int)reader["Car_id"],
+                            Is_passed = (int)reader["Is_passed"],
+
+                        };
+                        CarsByTests.Add(result);
+                    }
+                    my_logger.Info($"Got All Tests");
+                }
+            }
+        }
+    }
+}
